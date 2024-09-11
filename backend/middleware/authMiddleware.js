9205f5cebacbb2b9074856ee6f638dd9a5eee98e
sukritx <ddserial@gmail.com>
2024-09-11 19:21:17 +0700

    comment out console.error

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -26,7 +26,7 @@ const authMiddleware = async (req, res, next) => {
 
         next();
     } catch (error) {
-        console.error('Error in authMiddleware:', error);
+        // console.error('Error in authMiddleware:', error);
         res.status(401).json({ error: 'Please authenticate' });
     }
 };
