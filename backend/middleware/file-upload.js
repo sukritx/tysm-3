0a35270b9749c4de65f9df428b15c0fd541db0ab
sukritx <ddserial@gmail.com>
2024-10-13 17:21:42 +0700

    compress img before upload to spaces

--- backend/middleware/file-upload.js
+++ backend/middleware/file-upload.js
@@ -1,6 +1,7 @@
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const { S3Client } = require("@aws-sdk/client-s3");
+const sharp = require("sharp");
 require('dotenv').config();
 
 const s3Client = new S3Client({
@@ -15,7 +16,7 @@ const s3Client = new S3Client({
 
 const MIME_TYPE_MAP = {
   'image/png': 'png',
-  'image/jpeg': 'jpeg',
+  'image/jpeg': 'jpg',
   'image/jpg': 'jpg'
 };
 
@@ -35,13 +36,50 @@ const fileUpload = (options = {}) => {
       key: function (req, file, cb) {
         const ext = MIME_TYPE_MAP[file.mimetype];
         const userId = req.userId;
-        const fileName = `${userId}/${destination}/${Date.now()}.${ext}`;
+        const fileName = `${userId}/${destination}/${Date.now()}`;
         cb(null, fileName);
       },
+      contentType: multerS3.AUTO_CONTENT_TYPE,
       metadata: function (req, file, cb) {
         cb(null, { fieldName: file.fieldname });
       },
-      contentType: multerS3.AUTO_CONTENT_TYPE,
+      shouldTransform: function (req, file, cb) {
+        cb(null, /^image/i.test(file.mimetype))
+      },
+      transforms: [
+        {
+          id: 'webp',
+          key: function (req, file, cb) {
+            const userId = req.userId;
+            const fileName = `${userId}/${destination}/${Date.now()}.webp`;
+            cb(null, fileName);
+          },
+          transform: function (req, file, cb) {
+            cb(null, sharp()
+              .resize({ width: 1000, height: 1000, fit: 'inside' })
+              .webp({ quality: 80 })
+            )
+          }
+        },
+        {
+          id: 'original',
+          key: function (req, file, cb) {
+            const ext = MIME_TYPE_MAP[file.mimetype];
+            const userId = req.userId;
+            const fileName = `${userId}/${destination}/${Date.now()}.${ext}`;
+            cb(null, fileName);
+          },
+          transform: function (req, file, cb) {
+            let transform = sharp().resize({ width: 1000, height: 1000, fit: 'inside' });
+            if (file.mimetype === 'image/png') {
+              transform = transform.png({ quality: 80 });
+            } else {
+              transform = transform.jpeg({ quality: 80 });
+            }
+            cb(null, transform);
+          }
+        }
+      ]
     }),
     fileFilter: (req, file, cb) => {
       const isValid = !!MIME_TYPE_MAP[file.mimetype];
